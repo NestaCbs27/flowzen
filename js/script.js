a055c04af8856785e5e1c063e9c7f33c21da6702
NestaCbs27 <97601193+NestaCbs27@users.noreply.github.com>
2023-07-21 17:37:23 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 $('.navbar-nav>li>a').on('click', function(){
     $('.navbar-collapse').collapse('hide');
   });
+  
+ window.onload = function(){
+    document.querySelector('#input-color').addEventListener('change', function() {
+        var r = document.querySelector(':root');
+        Array.from(document.querySelectorAll('.update-fill')).forEach(v => v.style.fill = this.value);
+        Array.from(document.querySelectorAll('.update-stroke')).forEach(v => v.style.stroke = this.value);
+        r.style.setProperty('--main-color', this.value);
+    });
+};
