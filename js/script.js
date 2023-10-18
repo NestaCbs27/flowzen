87bf8c223ef1e835008abdfd41b93d94832aebce
crouillebois fabien <FABB-49@hotmail.fr>
2023-10-18 22:43:03 +0200

    nav transition

--- js/script.js
+++ js/script.js
@@ -5,3 +5,14 @@
 function callPhoneNumber(){
   window.open('tel:0673052057', '_self');
 }
+
+$(window).scroll(function(){
+  var scroll = $(window).scrollTop();
+    if (scroll > 30){
+      $("#imgLogo").addClass('shrink');
+      $("#imgLogoTitle").addClass('img-title-mobile')
+    } else {
+      $("#imgLogo").removeClass('shrink');
+      $("#imgLogoTitle").removeClass('img-title-mobile')
+    }
+  });
